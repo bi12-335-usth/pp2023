c02dd2255f79fb9ddafc19869f3484bd787b42de
bi12-335-usth <nhanpc.v2@gmail.com>
2023-03-08 00:27:24 +0700

    Practical 1 rework

--- 1.student.mark.rework.py
+++ 1.student.mark.rework.py
@@ -1,64 +1,66 @@
-students = [] # Defined a dictionary to store student info
-courses = [] # Defined a dictionary to store courses info
-marks = [] # Defined a dictionary to store marks info
+students = {} # Defined a dictionary to store student info
+courses = {} # Defined a dictionary to store courses info
+marks = {} # Defined a dictionary to store marks info
 
-#Function for the input of students and courses:
-def Count_students():
-    return int(input("Enter number of students: "))
-def Count_courses():
-    return int(input("\nEnter number of courses: "))
-def Student_detail():
-    s_Name = input("\nEnter student name: ")
-    s_ID = input("\nEnter student ID: ")
-    s_DoB = input("\nEnter DoB of student: ")
-    return (s_Name, s_ID, s_DoB)
-def Course_detail():
-    c_Name = input("\nEnter course name: ")
-    c_ID = input("\nEnter course ID: ")
-    return (c_Name, c_ID)
+# Function to insert the number of students and courses: 
+def insert_students():
+    return int(input("Enter the number of student: "))
+def insert_courses():
+    return int(input("\nEnter the number of courses: "))
 
-#Function for the input of student's marks:
-def type_in_marks(course,students):
-    mark = int(input(f"\n(Enter the mark for the course{course['c_Name']}: )"))
-    course['mark'] = []
-    for student in students:
-        mark = int(input(f"(Student {student['s_Name']}: )"))
-        course['mark'][student['s_ID']] = mark
+# Function for the student + course info:
+def Student__details():
+    n_students = insert_students()
+    for i in range(n_students):
+        s_name = input("\nEnter student name: ")
+        s_id = input("\nEnter student id: ")
+        s_DoB = input("\nEnter student DoB: ")
+        students[s_id] = {"name": s_name, "id": s_id, "DoB": s_DoB}
 
+def Course_details():
+    n_courses = insert_courses()
+    for i in range(n_courses):
+        c_name = input("\nEnter the name of the course: ")
+        c_id = input("\nEnter the id of the course: ")
+        courses[c_id] = {"name": c_name, "id": c_id}
+
+# Function to insert mark of the course:
+def insert_mark():
+    c_id = input("\nEnter the id of the course again: ")
+    if c_id not in courses:
+        return
+    for s_id in students:
+        mark = float(input(f"\nEnter the mark of {students[s_id]['name']}:"))
+        if s_id not in marks:
+            marks[s_id] = {}
+            marks[s_id][c_id] = mark
 #Function to listing the students, courses:
 def list_course(courses):
-    for course in courses:
-        print(f"Course ID {course['c_ID']}: ,\n Name {course['c_Name']}: ")
+    for c_id in courses:
+       print(f"Courses name: {courses[c_id]['name']}")
 def list_student(students):
-    for student in students:
-        print(f"Student_ID {student['s_ID']}:, \nName {student['s_Name']}:, \nDoB {student['s_DoB']}: ")
-def show_course_mark(courses):
-    for s_ID in students:
-        if s_ID in marks and courses in marks[s_ID]:
-            print(f"(Students_ID: {students[s_ID]['s_Name']}: {marks[s_ID]['c_ID']}")
+    for s_id in students:
+        print(f"Student name: {students[s_id]['name']}")
+
+# Function to show marks of the given courses:
+def display_marks():
+    c_id = input("Course id: ")
+    if c_id not in courses:
+        return
+    for s_id in students:
+        if s_id in marks and c_id in marks[s_id]:
+            print(f"(students_id: {students[s_id]['name']}: {marks[s_id][c_id]})")
         else:
-            print(f"{students[s_ID]['s_Name']}: Not found!")
+            print(f"{students[s_id]['name']}: This student is not in here!? Try again")
+        return
 
-# Main function:
-n_students = Count_students()
-for i in range(n_students):
-    students += [Student_detail()]
-
-n_courses = Count_courses()
-for i in range(n_courses):
-    courses += [Course_detail()]
-
-for course in courses:
-    type_in_marks(course, students)
-    print("\nListing student:")
-    list_student(students)
-    print("Show mark: ")
-for course in courses:
-    show_course_mark(course)
-
-# there is a error in line 22 that said list must be in integer, not string and i quite don't know how to fix
-# i have tried but i still can't get it fix, i'm really sorry
-
+# Main program to run: 
+Student__details()
+Course_details()
+insert_mark()
+print("Listing students:")
+list_student(students)
+display_marks()
 
 
 
